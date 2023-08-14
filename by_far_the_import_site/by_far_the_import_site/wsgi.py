3686b3b4345d55e9dd640cc272978257837118c8
SimSams <danya221299@gmail.com>
2023-08-14 06:24:24 -0700

    add app important_site

---
+++ by_far_the_import_site/by_far_the_import_site/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for by_far_the_import_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'by_far_the_import_site.settings')
+
+application = get_wsgi_application()
